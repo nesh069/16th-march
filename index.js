6260dbe05ba7233c4ffa4a4853a6432fcb873e6b
Emmanuel Munene <emmanuel.munene@student.moringaschool.com>
2026-03-17 14:50:04 +0300

    This is an update

---
+++ index.js
@@ -0,0 +1,16 @@
+//let speed = 80;
+//let distance = 400;
+//console.log("Time:", distance / speed)
+
+
+let grade = 40;
+if (grade >=79)
+    console.log("grade = A");
+else if (grade >= 65)
+    console.log("grade = B");
+else if (grade >= 55)
+    console.log("grade = C");
+else if (grade >= 30)
+    console.log("grade = D");
+else
+    console.log("grade = E");
