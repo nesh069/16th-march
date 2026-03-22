cff45b0c743ea7028f05de957b1c62b0599eb314
Emmanuel Munene <emmanuel.munene@student.moringaschool.com>
2026-03-22 18:46:21 +0300

    assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+let speed = 80
+let distance = 400
+console.log("Time:", distance / speed)
+ 
+function shortname(Alice, Bob, James, John, Mary, Mark, Emmanuel) {
+    return students.filter(student => student.length <= 6);
+}
+let students = ["Alice", "Bob", "James", "John", "Mary", "Mark", "Emmanuel"]
+console.log(shortname(students))
